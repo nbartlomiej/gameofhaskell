8c9fd450ce23eec524b9f19bb106a19ee8b2b501
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-12-03 23:49:51 +0100

    Working game engine, specs
    Pure part of the program; mainly a function that determines what is the
    next state of a board according to Game of Life rules.

--- GameOfHaskell.hs
+++ GameOfHaskell.hs
@@ -1,4 +1,20 @@
 module GameOfHaskell where
+import Data.List
 
-helloWorld = "Hi there"
+type Cell = (Int, Int)
+type Board = [Cell]
 
+nextState :: Board -> Board
+nextState board =
+  let newborn ((x,y),n)  = (x,y) `notElem` board && n == 3
+      survivor ((x,y),n) = (x,y) `elem` board && (n==3 || n==4)
+  in  [ cell | (cell,n) <- (neighbours board), newborn (cell,n) || survivor (cell,n)]
+
+neighbours :: Board -> [(Cell, Int)]
+neighbours board =
+  let deltas = [
+         (-1,-1),(0,-1),(1,-1),
+         (-1, 0),(0, 0),(1, 0),
+         (-1, 1),(0, 1),(1, 1)]
+      allNeighbours = concat $ map (\(x,y) -> map (\(a,b) -> (x+a,y+b)) deltas ) board
+  in (map (\xs@(x:_) -> (x, length xs)) . group . sort) allNeighbours 
--- GameOfHaskellSpecs.hs
+++ GameOfHaskellSpecs.hs
@@ -1,10 +1,43 @@
 module GameOfHaskell.Specs where
 
-import GameOfHaskell
+import qualified GameOfHaskell as G
 import Test.Hspec
 
+containsAll :: Eq a => [a] -> [a] -> Bool
+list `containsAll` elements = all (\x->x == True) ( map (\x->x `elem` list) elements)
+
 specs :: Specs
-specs = describe "application" [
+specs = describe "Application" [
+    describe "Integration" [
+      it "Simulates Conway's Game of Life" (
+        let board = [(-1,0),(0,0), (1,0)]
+            next = G.nextState board
+            expectedResults =  [(0,1),(0,0),(0,-1)]
+        in  next `containsAll` expectedResults
+      )
+    ],
+    describe "Board" [
+      describe "neighbours" [
+        it "Calculates neighbours for a single cell" (
+          let expectedResults = [
+                ((-1,-1),1), ((-1,0),1), ((-1,1),1),
+                ((0,-1),1),  ((0,0),1),  ((0,1),1),
+                ((1,-1),1),  ((1,0),1),  ((1,1),1) ]
+          in  (G.neighbours [(0,0)]) `containsAll` expectedResults
+        ),
+        it "Calculates neighbours for multiple cells" (
+          G.neighbours [(0,0),(2,2)] `containsAll` [((1,1),2)]
+        )
+      ],
+      describe "nextState" [
+        it "Creates new cells" (
+          (1,1) `elem` (G.nextState [(0,0),(0,1),(1,0)])
+        ),
+        it "Removes dead cells" (
+          (0,0) `notElem` (G.nextState [(0,0)])
+        )
+      ]
+    ]
   ]
 
 main = hspec specs
