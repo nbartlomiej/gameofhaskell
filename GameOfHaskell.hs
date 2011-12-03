d34c2ea31af75dda3b34ce111bc01aa8fcd27988
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-12-03 23:51:43 +0100

    Basic output
    Outputting a portion of the infinite board to the screen. The cells are
    set up in such a way to form a glider.

--- GameOfHaskell.hs
+++ GameOfHaskell.hs
@@ -1,5 +1,6 @@
 module GameOfHaskell where
 import Data.List
+import Control.Monad
 
 type Cell = (Int, Int)
 type Board = [Cell]
@@ -18,3 +19,23 @@ neighbours board =
          (-1, 1),(0, 1),(1, 1)]
       allNeighbours = concat $ map (\(x,y) -> map (\(a,b) -> (x+a,y+b)) deltas ) board
   in (map (\xs@(x:_) -> (x, length xs)) . group . sort) allNeighbours 
+
+-- -- -- -- -- --
+
+main = play [(0,0),(0,1),(0,2),(1,2),(2,1)]
+
+play :: Board -> IO ()
+play board = do
+  display board
+  putStrLn "Press return to continue..."
+  getLine
+  play $ nextState board
+
+display :: Board -> IO ()
+display board = do
+  mapM_ (\x -> do
+    mapM_ (\y ->
+        putChar $ if (x,y) `elem` board then 'x' else ' '
+      ) [-10 .. 10]
+    putChar '\n'
+    ) [-10 .. 2]
